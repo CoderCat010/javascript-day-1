1052eaa9f9cea0e87d8ddd2bc318f0cd703a4420
CoderCat010 <nusraumme@gmail.com>
2025-11-11 13:32:37 +0600

    added if else condition

--- practice-1.js
+++ practice-1.js
@@ -1,8 +1,17 @@
 //* ======JavaScript conditional statedment=====
 
 // ?if
-let age = 14;
-if (age < 18){
-    console.log('you are teenager');
-}
+// let age = 14;
+// if (age < 18){
+//     console.log('you are teenager');
+// }
+
+// ? if else
+    // let age = 133
+    // if (age >= 18){
+    //     console.log('you are adult');
+    // }
+    // else{
+    //     console.log('you are teenage')
+    // }
 
